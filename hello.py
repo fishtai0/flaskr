0ef18176c21412a2d675cc0345990ecac7fa0601
Yuwei Tian <fishtai0@outlook.com>
2017-06-15 11:00:58 +0800

    2a: render template

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # hello.py
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
