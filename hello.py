4defc03e4eb52eae1c30d8273e961123aa7a96fa
Yuwei Tian <fishtai0@outlook.com>
2017-06-23 17:07:15 +0800

    2c: add 404 and 500 error handler

--- hello.py
+++ hello.py
@@ -16,3 +16,13 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
