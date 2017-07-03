c96fded9099ec2b63636090564a2e4794f620cd1
Yuwei Tian <fishtai0@outlook.com>
2017-07-03 16:03:03 +0800

    3b: add redirection and session

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # hello.py
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -19,12 +19,11 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
