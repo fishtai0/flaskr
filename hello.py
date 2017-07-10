c3195a75b482d8fca07fab879c56a3f43063eeab
Yuwei Tian <fishtai0@outlook.com>
2017-07-10 16:02:37 +0800

    4a: add peewee models

--- hello.py
+++ hello.py
@@ -5,11 +5,46 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+import peewee as pw
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '44617457d542163d10ada66726b31ef80a88ac1a41013ea5'
 bootstrap = Bootstrap(app)
+
+
+db = pw.SqliteDatabase("flaskr.db")
+
+
+class BaseModel(pw.Model):
+    class Meta:
+        database = db
+
+
+class Role(BaseModel):
+    name = pw.CharField(64, unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+    class Meta:
+        db_table = 'roles'
+
+
+class User(BaseModel):
+    username = pw.CharField(64, unique=True, index=True)
+    role = pw.ForeignKeyField(Role, related_name='users', null=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    class Meta:
+        db_table = 'users'
+
+
+def create_tables():
+    db.connect()
+    db.create_tables([Role, User])
 
 
 class NameForm(FlaskForm):
