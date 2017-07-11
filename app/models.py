fcd9cea34c48260cd116b292bc95b748386b15d0
Yuwei Tian <fishtai0@outlook.com>
2017-07-11 23:09:03 +0800

    models: add password hash to User model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 import peewee as pw
 
 from . import db
@@ -16,6 +18,18 @@ class Role(db.Model):
 class User(db.Model):
     username = pw.CharField(64, unique=True, index=True)
     role = pw.ForeignKeyField(Role, related_name='users', null=True)
+    password_hash = pw.CharField(128)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
