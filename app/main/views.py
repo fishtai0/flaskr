98a028a4f2334b87a6c3662143c5d78d283155b1
Yuwei Tian <fishtai0@outlook.com>
2017-07-11 22:51:17 +0800

    main: finish up <index> view function

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,6 @@ from flask import render_template, session, redirect, url_for
 
 from . import main
 from .forms import NameForm
-from .. import db
 from ..models import User
 
 
@@ -12,7 +11,17 @@ from ..models import User
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        # ...
+        user = (User.select()
+                .where(User.username == form.name.data)
+                .first())
+        if user is None:
+            user = User(username=form.name.data)
+            user.save()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('.index'))
     return render_template('index.html',
                            form=form, name=session.get('name'),
