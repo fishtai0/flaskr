3bae28c2424474b02fa9cc588db24305da47c81f
Yuwei Tian <fishtai0@outlook.com>
2017-07-12 14:01:28 +0800

    auth: add user registration page

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask_login import login_user, logout_user, login_required
 
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -25,3 +25,16 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        user.save()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
