1c00d326b4677c6fecaae7d25a8afaf9b4b32e0d
Yuwei Tian <fishtai0@outlook.com>
2017-07-13 09:40:57 +0800

    auth: add user confirmation by sending email but not activated
    add flask-mail package

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
 
-from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import login_user, logout_user, login_required  # , current_user
 
 from . import auth
 from ..models import User
+# from ..email import send_email
 from .forms import LoginForm, RegistrationForm
 
 
@@ -35,22 +36,48 @@ def register():
                     username=form.username.data,
                     password=form.password.data)
         user.save()
+        # token = user.generate_confirmation_token()
+        # send_email(user.email, 'Confirm Your Account',
+        #            'auth/email/confirm', user=user, token=token)
+        # flash('A confirmation email has been sent to you by email.')
         flash('You can now login.')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
 
 
-@auth.before_app_request
-def before_request():
-    if (current_user.is_authenticated()
-        and not current_user.confirmed
-        and request.endpoint[:5] != 'auth.'
-            and request.endpoint != 'static'):
-        return redirect(url_for('auth.unconfirmed'))
+# @auth.route('/confirm/<token>')
+# @login_required
+# def confirm(token):
+#     if current_user.confirmed:
+#         return redirect(url_for('main.index'))
+#     if current_user.confirm(token):
+#         flash('You have confirmed your account. Thanks!')
+#     else:
+#         flash('The confirmation link is invalid or has expired.')
+#     return redirect(url_for('main.index'))
 
 
-@auth.route('/unconfirmed')
-def unconfirmed():
-    if current_user.is_anonymous() or current_user.confirmed:
-        return redirect(url_for('main.index'))
-    return render_template('auth/unconfirmed.html')
+# @auth.before_app_request
+# def before_request():
+#     if (current_user.is_authenticated() and
+#         not current_user.confirmed and
+#         request.endpoint[:5] != 'auth.' and
+#             request.endpoint != 'static'):
+#         return redirect(url_for('auth.unconfirmed'))
+
+
+# @auth.route('/unconfirmed')
+# def unconfirmed():
+#     if current_user.is_anonymous() or current_user.confirmed:
+#         return redirect(url_for('main.index'))
+#     return render_template('auth/unconfirmed.html')
+
+
+# @auth.route('/confirm')
+# @login_required
+# def resend_confirmation():
+#     token = current_user.generate_confirmation_token()
+#     send_email(current_user.email, 'Confirm Your Account',
+#                'auth/email/confirm', user=current_user, token=token)
+#     flash('A new confirmation email has been sent to you by email.')
+#     return redirect(url_for('main.index'))
